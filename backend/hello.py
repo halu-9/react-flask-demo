a4af9dcccb0da8b9b6eb1f9afb0901d1d9b1503f
ryo <>
2025-03-28 16:23:24 +0900

    CORS設定を追加し、FlaskからのレスポンスをReactで表示する簡単な例を確認

--- backend/hello.py
+++ backend/hello.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS # It is necessary to import this module to enable CORS
 
 app = Flask(__name__)
+CORS(app) # This will enable CORS for all routes(only for development)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return {'message': 'Hello, Flask!'}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // flaskからのメッセージを格納するstate
+  const [flask_message, setFlaskMessage] = useState("");
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setFlaskMessage(data.message);
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
+    };
+    fetchMessage();
+  }, []);
 
   return (
     <>
@@ -28,8 +47,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div>{flask_message || "default text"}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
